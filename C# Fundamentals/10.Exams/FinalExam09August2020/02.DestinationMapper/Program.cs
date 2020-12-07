75fc0b7a7e1909a0ad3fa27c46377a17d0572e23
StoyanDermendzhiev <techo01@abv.bg>
2020-12-07 17:30:08 +0200

    Update

--- C# Fundamentals/10.Exams/FinalExam09August2020/02.DestinationMapper/Program.cs
+++ C# Fundamentals/10.Exams/FinalExam09August2020/02.DestinationMapper/Program.cs
@@ -11,16 +11,19 @@ namespace _02.DestinationMapper
             string places = Console.ReadLine();
             List<string> validLocations = new List<string>();
 
-            string pattern = @"([=|\/])(?<location>[A-z][A-Za-z]{2,})\1";
+            string pattern = @"(=|\/)(?<location>[A-Z][A-Za-z]{2,})\1";
             Regex regex = new Regex(pattern);
             MatchCollection validPlaces = regex.Matches(places);
 
             int sum = 0;
 
-            foreach (Match place in validPlaces)
+            if (validPlaces.Count > 0)
             {
-                validLocations.Add(place.Groups["location"].Value);
-                sum += place.Groups["location"].Length;
+                foreach (Match place in validPlaces)
+                {
+                    validLocations.Add(place.Groups["location"].Value);
+                    sum += place.Groups["location"].Length;
+                }
             }
 
             Console.WriteLine($"Destinations: {string.Join(", ", validLocations)}");
