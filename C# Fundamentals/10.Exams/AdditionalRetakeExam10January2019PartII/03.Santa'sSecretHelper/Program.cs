dad4d3fb3e2a61486bd32550c97ede6dbb92659e
StoyanDermendzhiev <techo01@abv.bg>
2020-12-08 18:24:20 +0200

    Exam prep

---
+++ C# Fundamentals/10.Exams/AdditionalRetakeExam10January2019PartII/03.Santa'sSecretHelper/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _03.Santa_sSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            
+            string message = Console.ReadLine();
+
+            string pattern = @"@(?<name>[A-Za-z]+)([^@\-!:>]+)!(?<behavior>[G|N])!";
+            Regex regex = new Regex(pattern);
+
+            var sb = new StringBuilder();
+
+            while (message != "end")
+            {
+                string decryptedMessage = string.Empty;
+
+                foreach (char ch in message)
+                {
+                    decryptedMessage += (char)(ch - key);
+                }
+
+                Match match = regex.Match(decryptedMessage);
+
+                if (match.Success && match.Groups["behavior"].Value == "G")
+                {
+                    sb.AppendLine(match.Groups["name"].Value);
+                }
+
+                message = Console.ReadLine();
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
---
+++ C# Fundamentals/10.Exams/AdditionalRetakeExam10January2019PartII/04.Santa'sNewList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Santa_sNewList
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> children = new Dictionary<string, int>();
+            Dictionary<string, int> toys = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] childInfo = input.Split("->");
+
+                if (childInfo[0] != "Remove")
+                {
+                    string childName = childInfo[0];
+                    string toyType = childInfo[1];
+                    int amount = int.Parse(childInfo[2]);
+
+                    if (children.ContainsKey(childName))
+                    {
+                        children[childName] += amount;
+                    }
+                    else
+                    {
+                        children.Add(childName, amount);
+                    }
+
+                    if (toys.ContainsKey(toyType))
+                    {
+                        toys[toyType] += amount;
+                    }
+                    else
+                    {
+                        toys.Add(toyType, amount);
+                    }
+                }
+                else
+                {
+                    string childName = childInfo[1];
+                    children.Remove(childName);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Children:");
+
+            foreach (var child in children.OrderByDescending(x=>x.Value)
+                                          .ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{child.Key} -> {child.Value}");
+            }
+
+            Console.WriteLine("Presents:");
+
+            foreach (var toy in toys)
+            {
+                Console.WriteLine($"{toy.Key} -> {toy.Value}");
+            }
+        }
+    }
+}
