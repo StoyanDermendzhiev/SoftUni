cc56dd38aa2bd2fd50b706238f18e1dc355892c1
StoyanDermendzhiev <techo01@abv.bg>
2020-12-09 19:51:35 +0200

    Exam prep

---
+++ C# Fundamentals/11.StringsAndRegularExpressionsMoreExercises/07.Hideout/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _07.Hideout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string map = Console.ReadLine();
+
+            while (true)
+            {
+                string[] clues = Console.ReadLine()
+                                        .Split();
+
+                string hideout = Regex.Escape(clues[0]);
+                int minSize = int.Parse(clues[1]);
+
+                string pattern = $@"[{hideout}]+";
+
+                foreach (Match item in Regex.Matches(map, pattern))
+                {
+                    if (item.Length >= minSize)
+                    {
+                        Console.WriteLine($"Hideout found at index {item.Index} and it is with size {item.Length}!");
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
