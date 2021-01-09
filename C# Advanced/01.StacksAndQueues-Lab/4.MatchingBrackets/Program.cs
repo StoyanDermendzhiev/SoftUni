1356f31d1d2ec3808c6c61af7b1498c268e0a8d2
StoyanDermendzhiev <techo01@abv.bg>
2021-01-09 10:54:13 +0200

    Adding Problems

---
+++ C# Advanced/01.StacksAndQueues-Lab/4.MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int length = i - startIndex + 1;
+
+                    Console.WriteLine(expression.Substring(startIndex, length));
+                }
+            }
+        }
+    }
+}
