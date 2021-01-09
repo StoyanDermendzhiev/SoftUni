6bbd8fe38b29fd3e4b56ac93f6f8fdccd9e9e8f5
StoyanDermendzhiev <techo01@abv.bg>
2021-01-09 14:47:50 +0200

    Upload

---
+++ C# Advanced/01.StacksAndQueues-Exercise/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                                .Split();
+            var elements = Console.ReadLine()
+                                  .Split()
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            int s = int.Parse(numbers[1]);
+            int x = int.Parse(numbers[2]);
+
+            var queue = new Queue<int>(elements);
+
+            for (int i = 0; i < s; i++)
+            {
+                if (queue.Count == 0)
+                {
+                    break;
+                }
+                queue.Dequeue();
+            }
+
+            if (queue.Count != 0)
+            {
+                if (queue.Contains(x))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
