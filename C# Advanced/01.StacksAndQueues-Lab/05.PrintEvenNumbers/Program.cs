14baf31efd20bf5aacfe63a6e258b6ff327fd10d
StoyanDermendzhiev <techo01@abv.bg>
2021-01-09 11:09:44 +0200

    Adding Problem

---
+++ C# Advanced/01.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                                 .Split()
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            var queue = new Queue<int>();
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    queue.Enqueue(num);
+                }
+            }
+
+            while (queue.Count > 1)
+            {
+                Console.Write($"{queue.Dequeue()}, ");
+            }
+
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
