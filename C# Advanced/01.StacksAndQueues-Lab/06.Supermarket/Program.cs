38627d17600e0a4ca1ba24be9cc6ba38bbff6200
StoyanDermendzhiev <techo01@abv.bg>
2021-01-09 14:03:06 +0200

    Add problem

---
+++ C# Advanced/01.StacksAndQueues-Lab/06.Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+            string input = Console.ReadLine();
+
+            var sb = new StringBuilder();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count > 0)
+                    {
+                        sb.AppendLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            sb.AppendLine($"{queue.Count} people remaining");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
