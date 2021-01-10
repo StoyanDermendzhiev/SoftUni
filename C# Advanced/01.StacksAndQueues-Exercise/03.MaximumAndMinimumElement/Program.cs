9d4fcf386c06028e1caf0aee4110bceef9ac62d1
StoyanDermendzhiev <techo01@abv.bg>
2021-01-10 09:43:01 +0200

    Upload

---
+++ C# Advanced/01.StacksAndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var stack = new Stack<int>();
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                int query = int.Parse(input[0]);
+
+                switch (query)
+                {
+                    case 1:
+                        int x = int.Parse(input[1]);
+                        stack.Push(x);
+                        break;
+                    case 2:
+                        stack.Pop();
+                        break;
+                    case 3:
+                        if (stack.Count > 0)
+                        {
+                            sb.AppendLine(stack.Max().ToString());
+                        }
+                        break;
+                    case 4:
+                        if (stack.Count > 0)
+                        {
+                            sb.AppendLine(stack.Min().ToString());
+                        }
+                        break;
+                }
+            }
+
+            while (stack.Count > 1)
+            {
+                sb.Append($"{stack.Pop()}, ");
+            }
+
+            sb.Append(stack.Pop().ToString());
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
