5fd43d1a8966fd93627ab4236461c496530efee7
StoyanDermendzhiev <techo01@abv.bg>
2021-01-10 11:47:42 +0200

    Upload

---
+++ C# Advanced/01.StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songs = Console.ReadLine().Split(", ");
+
+            var queue = new Queue<string>(songs);
+
+            string input = Console.ReadLine();
+
+            while (queue.Count > 0)
+            {
+                string[] commandInfo = input.Split();
+                string command = commandInfo[0];
+
+                if (command == "Play")
+                {
+                    queue.Dequeue();
+                }
+                else if (command == "Add")
+                {
+                    string song = input.Remove(0, command.Length + 1);
+                    if (queue.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(song);
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", queue));
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
