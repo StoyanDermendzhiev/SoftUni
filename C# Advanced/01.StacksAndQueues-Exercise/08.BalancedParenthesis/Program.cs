3058113f3ed4e2b20b3de3434b281be32d732b00
StoyanDermendzhiev <techo01@abv.bg>
2021-01-13 20:42:24 +0200

    Upload

---
+++ C# Advanced/01.StacksAndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sequence = Console.ReadLine()
+                                     .Trim()
+                                     .ToCharArray();
+
+            var stack = new Stack<char>();
+
+            bool isBalanced = true;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                char currentChar = sequence[i];
+                if (currentChar == '(' || currentChar == '{' || currentChar == '[')
+                {
+                    stack.Push(currentChar);
+                }
+                else if (currentChar == ')' || currentChar == '}' || currentChar == ']')
+                {
+                    char previosChar = stack.Peek();
+
+                    if ((previosChar == '(' && currentChar == ')') ||
+                       (previosChar == '{' && currentChar == '}') ||
+                       (previosChar == '[' && currentChar == ']'))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isBalanced && stack.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
