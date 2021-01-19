b01646f5f7927a12d52652ba8549612085a268f7
StoyanDermendzhiev <techo01@abv.bg>
2021-01-19 19:34:30 +0200

    Upload

---
+++ C# Advanced/02.MultidimensionalArrays-Exercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine()
+                                       .Split()
+                                       .Select(int.Parse)
+                                       .ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int primDiagonalSum = 0;
+            int secDiagonalSum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                primDiagonalSum += matrix[row, row];
+                secDiagonalSum += matrix[row, n - 1 - row];
+            }
+
+            int diff = Math.Abs(primDiagonalSum - secDiagonalSum);
+
+            Console.WriteLine(diff);
+        }
+    }
+}
