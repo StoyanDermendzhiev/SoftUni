4d466267ce53eeed3c45b358b81984f464221a92
StoyanDermendzhiev <techo01@abv.bg>
2021-01-20 18:08:13 +0200

    Modify

--- C# Advanced/02.MultidimensionalArrays-Exercise/05.SnakeMoves/Program.cs
+++ C# Advanced/02.MultidimensionalArrays-Exercise/05.SnakeMoves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05.SnakeMoves
 {
@@ -6,7 +7,64 @@ namespace _05.SnakeMoves
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] matrixDimensions = Console.ReadLine()
+                                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                            .Select(int.Parse)
+                                            .ToArray();
+            string snake = Console.ReadLine();
+
+            int n = matrixDimensions[0];
+            int m = matrixDimensions[1];
+
+            char[,] matrix = new char[n, m];
+
+            int count = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                                
+                if (row % 2 == 0)
+                {
+                    for (int col = 0; col < m; col++)
+                    {
+                        if (count == snake.Length)
+                        {
+                            count = 0;
+                        }
+
+                        matrix[row, col] = snake[count];
+                        count++;
+                    }
+                }
+                else
+                {
+                    for (int col = m - 1; col >= 0; col--)
+                    {
+                        if (count == snake.Length)
+                        {
+                            count = 0;
+                        }
+
+                        matrix[row, col] = snake[count];
+                        count++;
+                    }
+                }
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
