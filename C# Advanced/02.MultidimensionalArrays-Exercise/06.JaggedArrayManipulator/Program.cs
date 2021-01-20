2503d298af45a9e7d2d228f31cbeb91978dba559
StoyanDermendzhiev <techo01@abv.bg>
2021-01-20 18:08:42 +0200

    Upload

---
+++ C# Advanced/02.MultidimensionalArrays-Exercise/06.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.JaggedArrayManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] jaggedMatrix = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                jaggedMatrix[row] = Console.ReadLine()
+                                           .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                           .Select(int.Parse)
+                                           .ToArray();
+            }
+
+            for (int row = 0; row < n - 1; row++)
+            {
+                if (jaggedMatrix[row].Length == jaggedMatrix[row + 1].Length)
+                {
+                    for (int col = 0; col < jaggedMatrix[row].Length; col++)
+                    {
+                        jaggedMatrix[row][col] *= 2;
+                        jaggedMatrix[row + 1][col] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < jaggedMatrix[row].Length; col++)
+                    {
+                        jaggedMatrix[row][col] /= 2;
+                    }
+                    for (int col = 0; col < jaggedMatrix[row + 1].Length; col++)
+                    {
+                        jaggedMatrix[row + 1][col] /= 2;
+                    }
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] commandInfo = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = commandInfo[0];
+                int row = int.Parse(commandInfo[1]);
+                int col = int.Parse(commandInfo[2]);
+                int value = int.Parse(commandInfo[3]);
+
+                bool isValidIndexes = row >= 0 && row < n &&
+                                      col >= 0 && col < jaggedMatrix[row].Length;
+
+                if (isValidIndexes)
+                {
+                    switch (command)
+                    {
+                        case "Add":
+                            jaggedMatrix[row][col] += value;
+                            break;
+                        case "Subtract":
+                            jaggedMatrix[row][col] -= value;
+                            break;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            PrintMatrix(jaggedMatrix);
+        }
+
+        static void PrintMatrix(int[][] matrix)
+        {
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
