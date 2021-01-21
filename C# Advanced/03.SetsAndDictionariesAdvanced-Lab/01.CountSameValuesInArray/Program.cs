b7e322c4004ab7593058a7dddc4a3497a48bfd5d
StoyanDermendzhiev <techo01@abv.bg>
2021-01-21 20:47:03 +0200

    Upload

---
+++ C# Advanced/03.SetsAndDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<double, int> counts = new Dictionary<double, int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double number = double.Parse(numbers[i]);
+
+                if (!counts.ContainsKey(number))
+                {
+                    counts.Add(number, 0);
+                }
+
+                counts[number] += 1;
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} - {number.Value} times");
+            }
+        }
+    }
+}
