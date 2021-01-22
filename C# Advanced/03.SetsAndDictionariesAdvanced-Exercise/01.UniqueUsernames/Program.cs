c239338d05364251ea242807b511d95f2dae6b40
StoyanDermendzhiev <techo01@abv.bg>
2021-01-22 19:07:31 +0200

    Upload

---
+++ C# Advanced/03.SetsAndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, uniqueNames));
+        }
+    }
+}
