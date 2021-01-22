b6c92a3cb6a56e0ea53f57db29d276f8ee195462
StoyanDermendzhiev <techo01@abv.bg>
2021-01-22 18:07:07 +0200

    Upload

---
+++ C# Advanced/03.SetsAndDictionariesAdvanced-Lab/05.RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int namesNumber = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < namesNumber; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
