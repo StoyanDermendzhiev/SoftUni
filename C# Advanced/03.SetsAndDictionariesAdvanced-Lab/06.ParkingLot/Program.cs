6a5c9b3925d3adf9e2f48b49e7be4ba631e3e14c
StoyanDermendzhiev <techo01@abv.bg>
2021-01-22 18:07:43 +0200

    Upload

---
+++ C# Advanced/03.SetsAndDictionariesAdvanced-Lab/06.ParkingLot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> cars = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] carInfo = input.Split(", ");
+                string command = carInfo[0];
+                string carNumber = carInfo[1];
+
+                if (command == "IN")
+                {
+                    cars.Add(carNumber);
+                }
+                else if (command == "OUT")
+                {
+                    cars.Remove(carNumber);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
