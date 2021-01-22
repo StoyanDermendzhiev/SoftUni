03233c9e2054ed35c3d37f14dc9fd437f5a95943
StoyanDermendzhiev <techo01@abv.bg>
2021-01-22 18:08:14 +0200

    Upload

---
+++ C# Advanced/03.SetsAndDictionariesAdvanced-Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reservationNumber = Console.ReadLine();
+
+            HashSet<string> vips = new HashSet<string>();
+            HashSet<string> regulars = new HashSet<string>();
+
+            while (reservationNumber != "PARTY")
+            {
+                if (char.IsDigit(reservationNumber[0]))
+                {
+                    vips.Add(reservationNumber);
+                }
+                else
+                {
+                    regulars.Add(reservationNumber);
+                }
+
+                reservationNumber = Console.ReadLine();
+            }
+
+            string partyPerson = Console.ReadLine();
+
+            while (partyPerson != "END")
+            {
+                if (vips.Contains(partyPerson))
+                {
+                    vips.Remove(partyPerson);
+                }
+                else if (regulars.Contains(partyPerson))
+                {
+                    regulars.Remove(partyPerson);
+                }
+
+                partyPerson = Console.ReadLine();
+            }
+
+            Console.WriteLine(vips.Count + regulars.Count);
+
+            foreach (var vip in vips)
+            {
+                Console.WriteLine(vip);
+            }
+
+            foreach (var regular in regulars)
+            {
+                Console.WriteLine(regular);
+            }
+        }
+    }
+}
