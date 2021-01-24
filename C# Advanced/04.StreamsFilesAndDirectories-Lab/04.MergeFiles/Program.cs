47298ff45a8c33d0340b6a6aec2f8054657e2a52
StoyanDermendzhiev <techo01@abv.bg>
2021-01-24 17:16:37 +0200

    Modify

---
+++ C# Advanced/04.StreamsFilesAndDirectories-Lab/04.MergeFiles/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] readFirstFile = File.ReadAllLines("../../../FileOne.txt");
+            string[] readSecondFile = File.ReadAllLines("../../../FileTwo.txt");
+            File.WriteAllText("../../../Output.txt", "");
+
+            for (int i = 0; i < readFirstFile.Length; i++)
+            {
+                File.AppendAllText("../../../output.txt", readFirstFile[i] + Environment.NewLine + 
+                                                          readSecondFile[i] + Environment.NewLine);
+            }
+        }
+    }
+}
