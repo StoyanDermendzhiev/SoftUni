86e5a685c12ff075dca40a1fd043e558d61eb4e4
StoyanDermendzhiev <techo01@abv.bg>
2021-01-24 11:25:31 +0200

    Upload

---
+++ C# Advanced/04.StreamsFilesAndDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string currentRow = reader.ReadLine();
+                int row = 1;
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (currentRow != null)
+                    {
+                        writer.WriteLine($"{row}. {currentRow}");
+
+                        currentRow = reader.ReadLine();
+                        row++;
+                    }
+                }
+            }
+        }
+    }
+}
