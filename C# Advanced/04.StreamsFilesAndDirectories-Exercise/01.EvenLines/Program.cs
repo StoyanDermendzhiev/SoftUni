b125df4c242414a398103ddf9edf6f9cc7a7fd06
StoyanDermendzhiev <techo01@abv.bg>
2021-01-26 21:16:04 +0200

    Modify

---
+++ C# Advanced/04.StreamsFilesAndDirectories-Exercise/01.EvenLines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                string line = reader.ReadLine();
+                int counter = 0;
+
+                string pattern = @"[\-,.!?]";
+                Regex regex = new Regex(pattern);
+
+                while (line != null)
+                {
+                    if (counter++ % 2 == 0)
+                    {
+                        line = regex.Replace(line, "@");
+                        string[] words = line.Split();
+
+                        Console.WriteLine(string.Join(" ", words.Reverse()));
+                    }
+
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
