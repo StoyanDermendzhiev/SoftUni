ac6f96bea3fb9686c00e213e06d29c1104773443
StoyanDermendzhiev <techo01@abv.bg>
2021-01-27 22:04:43 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Func<string, bool> checkIsUpper = n => char.IsUpper(n[0]);
+
+            string[] words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Where(checkIsUpper)
+                                 .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
