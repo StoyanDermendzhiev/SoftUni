ab6d885c10b2e4249f488fbebd07043c5a76e8bd
StoyanDermendzhiev <techo01@abv.bg>
2021-02-01 17:54:21 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToList();
+
+            int n = int.Parse(Console.ReadLine());
+
+            Predicate<int> isDivisible = num => num % n == 0;
+
+            numbers.RemoveAll(isDivisible);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
