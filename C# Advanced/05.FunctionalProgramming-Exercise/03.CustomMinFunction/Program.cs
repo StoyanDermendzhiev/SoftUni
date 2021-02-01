0f852aed9648ec6c2c519add733afa0e64268211
StoyanDermendzhiev <techo01@abv.bg>
2021-02-01 17:52:53 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> getMinNumber = numbers =>
+            {
+                int minNumber = int.MaxValue;
+
+                foreach (var num in numbers)
+                {
+                    if (minNumber > num)
+                    {
+                        minNumber = num;
+                    }
+                }
+
+                return minNumber;
+            };
+
+            Console.WriteLine(getMinNumber(numbers));
+        }
+    }
+}
