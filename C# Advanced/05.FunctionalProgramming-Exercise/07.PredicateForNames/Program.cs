588f72d7614838f289b4ec647db55b35509eb8a6
StoyanDermendzhiev <techo01@abv.bg>
2021-02-01 17:54:46 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<List<string>, List<string>> filter = names =>
+            {
+                names = names.Where(name => name.Length <= 4).ToList();
+
+                return names;
+            };
+
+            Console.WriteLine(string.Join(Environment.NewLine, filter(names)));
+        }
+    }
+}
