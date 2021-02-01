54ecdfc7e4134210f3eeb9e81fe664d77c0fe065
StoyanDermendzhiev <techo01@abv.bg>
2021-02-01 17:50:11 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Action<string[]> printNames = names => 
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printNames(names);
+        }
+    }
+}
