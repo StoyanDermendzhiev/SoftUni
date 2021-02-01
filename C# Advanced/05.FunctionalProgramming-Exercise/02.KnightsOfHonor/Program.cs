d1f61292f00f25d184904eb7be2d7feb157cd788
StoyanDermendzhiev <techo01@abv.bg>
2021-02-01 17:50:30 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] knights = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(knight => $"Sir {knight}")
+                .ToArray();
+
+            Action<string[]> printKnights = names =>
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printKnights(knights);
+        }
+    }
+}
