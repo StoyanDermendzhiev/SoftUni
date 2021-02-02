982c613d03d5e67d30b1984160f12e37757ea89b
StoyanDermendzhiev <techo01@abv.bg>
2021-02-02 19:30:43 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/11.ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.ThePartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            List<string> filters = new List<string>();
+
+            while (input != "Print")
+            {
+                string[] commandInfo = input.Split(";");
+                string command = commandInfo[0];
+                string filterType = commandInfo[1];
+                string filterParameter = commandInfo[2];
+
+                if (command == "Add filter")
+                {
+                    filters.Add(filterType + " " + filterParameter);
+                }
+                else if (command == "Remove filter")
+                {
+                    filters.Remove(filterType + " " + filterParameter); ;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var filter in filters)
+            {
+                var commands = filter.Split(' ');
+
+                if (commands[0] == "Starts")
+                {
+                    names = names.Where(p => !p.StartsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Ends")
+                {
+                    names = names.Where(p => !p.EndsWith(commands[2])).ToList();
+                }
+                else if (commands[0] == "Length")
+                {
+                    names = names.Where(p => p.Length != int.Parse(commands[1])).ToList();
+                }
+                else if (commands[0] == "Contains")
+                {
+                    names = names.Where(p => !p.Contains(commands[1])).ToList();
+                }
+            }
+
+            if (names.Any())
+            {
+                Console.WriteLine(string.Join(" ", names));
+            }
+        }
+                    }
+}
