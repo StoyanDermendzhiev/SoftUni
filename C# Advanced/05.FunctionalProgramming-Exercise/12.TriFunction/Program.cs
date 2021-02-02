b97a31b7db0204a4db5e4f683a4de98c157c4a6d
StoyanDermendzhiev <techo01@abv.bg>
2021-02-02 19:31:07 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/12.TriFunction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> filter = (name, n) =>
+            {
+                int sum = 0;
+
+                foreach (char ch in name)
+                {
+                    sum += ch;
+                }
+
+                if (sum >= n)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            Func<List<string>, int, Func<string, int, bool>, string> result
+                = (names, n, filter) => names.FirstOrDefault(name => filter(name, n));
+
+            string name = result(names, n, filter);
+
+            Console.WriteLine(name);
+        }
+    }
+}
