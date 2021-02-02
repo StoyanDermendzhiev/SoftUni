7547a39e6775ce83b2d261cf8448ee7b3204e739
StoyanDermendzhiev <techo01@abv.bg>
2021-02-02 19:29:52 +0200

    Upload

---
+++ C# Advanced/05.FunctionalProgramming-Exercise/09.ListOfPredicates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            List<int> numbers = new List<int>();
+
+            Func<int, int, bool> predicate = (num, div) => num % div == 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (dividers.All(d => predicate(i, d)))
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
