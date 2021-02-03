c5062a27ec3dfb65bfd6393a10c09b2024c17fb4
StoyanDermendzhiev <techo01@abv.bg>
2021-02-03 21:43:03 +0200

    Upload

---
+++ C# Advanced/06.DefiningClasses-Lab/02.CarExtension/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive (double distance)
+        {
+            double fuelNeeded = FuelConsumption * distance;
+
+            if (FuelQuantity - fuelNeeded >= 0)
+            {
+                FuelQuantity -= fuelNeeded;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"Fuel: {FuelQuantity:f2}");
+
+            return sb.ToString();
+        }
+    }
+}
