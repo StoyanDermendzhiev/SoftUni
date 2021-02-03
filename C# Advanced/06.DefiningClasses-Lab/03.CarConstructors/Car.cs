d7523c72eb3968d8f3cbadd1335ebeba6a892c22
StoyanDermendzhiev <techo01@abv.bg>
2021-02-03 21:43:33 +0200

    Upload

---
+++ C# Advanced/06.DefiningClasses-Lab/03.CarConstructors/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+       public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+        
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive (double distance)
+        {
+            double fuelNeeded = FuelConsumption * distance;
+
+            if (FuelQuantity - fuelNeeded >= 0)
+            {
+                FuelQuantity -= fuelNeeded;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"Year: {Year}");
+            sb.AppendLine($"Fuel: {FuelQuantity:f2}");
+
+            return sb.ToString();
+        }
+    }
+}
