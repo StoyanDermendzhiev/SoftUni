3d4a4146f99ba5c47538d82edb468337b2a5aaec
StoyanDermendzhiev <techo01@abv.bg>
2021-02-04 21:10:08 +0200

    Upload

---
+++ C# Advanced/06.DefiningClasses-Exercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family(List<Person> people)
+        {
+            People = new List<Person>();
+        }
+
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = People.OrderByDescending(p => p.Age)
+                                        .FirstOrDefault();
+
+            return oldestMember;
+        }
+    }
+}
---
+++ C# Advanced/06.DefiningClasses-Exercise/03.OldestFamilyMember/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family(new List<Person>());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                
+                Person person = new Person(personInfo[0], int.Parse(personInfo[1]));
+
+                family.AddMember(person);
+            }
+
+            Person oldestMember = new Person();
+            oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
