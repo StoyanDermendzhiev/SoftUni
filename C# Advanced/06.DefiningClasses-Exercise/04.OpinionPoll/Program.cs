919386954d249f1a4d287adb7e366882bcfe2874
StoyanDermendzhiev <techo01@abv.bg>
2021-02-04 21:10:32 +0200

    Upload

---
+++ C# Advanced/06.DefiningClasses-Exercise/04.OpinionPoll/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] personData = Console.ReadLine().Split();
+
+                Person person = new Person(personData[0], int.Parse(personData[1]));
+                people.Add(person);
+            }
+
+            foreach (Person person in people.Where(p => p.Age > 30)
+                                            .OrderBy(p => p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
