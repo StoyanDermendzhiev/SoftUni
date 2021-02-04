3739f200a1a970f2405fca2bf68f56ba65820bea
StoyanDermendzhiev <techo01@abv.bg>
2021-02-04 21:10:55 +0200

    Upload

---
+++ C# Advanced/06.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public static class DateModifier
+    {
+        public static int CalculateDiference(string startDate, string endDate)
+        {
+            DateTime start = DateTime.Parse(startDate);
+            DateTime end = DateTime.Parse(endDate);
+
+            TimeSpan difference = start - end;
+
+            return Math.Abs(difference.Days);
+        }
+    }
+}
