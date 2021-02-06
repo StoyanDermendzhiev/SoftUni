7702c09d3c9e3376ed729cda10ce3e520f56ed55
StoyanDermendzhiev <techo01@abv.bg>
2021-02-06 12:09:16 +0200

    Upload

---
+++ C# Advanced/07.ImplementingLinkedList-Lab/01.CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddLast(i);
+            }
+
+            Console.WriteLine(list.RemoveFirst());
+            Console.WriteLine(list.RemoveLast());
+            Console.WriteLine();
+            list.ForEach(n => Console.WriteLine(n));
+
+            int[] listToArray = list.ToArray();
+
+            list.AddFirst(100);
+            list.AddLast(100);
+
+            list.ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
