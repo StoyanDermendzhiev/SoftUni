679897f7c9659029c569a2101a118860630a40c8
StoyanDermendzhiev <techo01@abv.bg>
2021-02-07 20:24:45 +0200

    Modify

--- C# Advanced/07.ImplementingCustomListAndStack/02.CustomStack/Program.cs
+++ C# Advanced/07.ImplementingCustomListAndStack/02.CustomStack/Program.cs
@@ -6,13 +6,13 @@ namespace _02.CustomStack
     {
         static void Main(string[] args)
         {
-            CustomStack stack = new CustomStack();
+            CustomStack<string> stack = new CustomStack<string>();
 
-            stack.Push(10);
-            stack.Push(20);
-            stack.Push(30);
-            stack.Push(40);
-            stack.Push(50);
+            stack.Push("10");
+            stack.Push("20");
+            stack.Push("30");
+            stack.Push("40");
+            stack.Push("50");
             stack.Peek();
             stack.Pop();
             stack.ForEach(s => Console.WriteLine(s));
