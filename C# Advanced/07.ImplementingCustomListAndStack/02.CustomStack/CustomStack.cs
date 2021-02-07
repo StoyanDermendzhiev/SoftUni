016dcf7d3ef6b2fda3936af915f0009043c42e8b
StoyanDermendzhiev <techo01@abv.bg>
2021-02-07 18:59:14 +0200

    Upload

---
+++ C# Advanced/07.ImplementingCustomListAndStack/02.CustomStack/CustomStack.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.CustomStack
+{
+    class CustomStack
+    {
+        private const int InitialCapacity = 4;
+
+        private int[] items;
+
+        private int count;
+
+        public CustomStack()
+        {
+            count = 0;
+            items = new int[InitialCapacity];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public void Push(int element)
+        {
+            if (items.Length == count)
+            {
+                var nextItems = new int[items.Length * 2];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    nextItems[i] = items[i];
+                }
+
+                items = nextItems;
+            }
+
+            items[count] = element;
+            count++;
+        }
+
+        public int Pop()
+        {
+            if (items.Length == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+
+            var lastIndex = count - 1;
+            int last = items[lastIndex];
+            count--;
+
+            if (count == items.Length / 2)
+            {
+                var previousItems = new int[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    previousItems[i] = items[i];
+                }
+
+                items = previousItems;
+            }
+
+            return last;
+        }
+
+        public int Peek()
+        {
+            if (items.Length == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+
+            int peekElement = items[count - 1];
+
+            return peekElement;
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action(items[i]);
+            }
+        }
+    }
+}
---
+++ C# Advanced/07.ImplementingCustomListAndStack/02.CustomStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.CustomStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack stack = new CustomStack();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+            stack.Peek();
+            stack.Pop();
+            stack.ForEach(s => Console.WriteLine(s));
+        }
+    }
+}
