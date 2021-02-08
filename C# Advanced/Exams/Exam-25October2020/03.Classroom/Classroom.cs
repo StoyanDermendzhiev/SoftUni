44d7b6cf6ea3bbec78417afddd66eac2f0fff17f
StoyanDermendzhiev <techo01@abv.bg>
2021-02-08 18:54:22 +0200

    Upload

---
+++ C# Advanced/Exams/Exam-25October2020/03.Classroom/Classroom.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => students.Count;
+
+        public string RegisterStudent(Student student)
+        {
+            if (Count < Capacity)
+            {
+                students.Add(student);
+                return $"Added student {student.FirstName} {student.LastName}";
+            }
+            else
+            {
+                return "No seats in the classroom";
+            }
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student currStudent = students.FindAll(s => s.FirstName == firstName).Find(s=>s.LastName == lastName);
+            if (currStudent != null)
+            {
+                students.Remove(currStudent);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+            else
+            {
+                return "Student not found";
+            }
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            List<Student> studentsForSubject = students.FindAll(s => s.Subject == subject);
+
+            if (studentsForSubject.Count > 0)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine("Students:");
+
+                foreach (var student in studentsForSubject)
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+
+                return sb.ToString().Trim();
+            }
+            else
+            {
+                return "No students enrolled for the subject";
+            }
+        }
+
+        public int GetStudentsCount()
+        {
+            return students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            return students.FindAll(s => s.FirstName == firstName).Find(s => s.LastName == lastName);
+        }
+    }
+}
