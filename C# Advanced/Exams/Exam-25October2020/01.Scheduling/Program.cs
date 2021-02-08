8f6e1f03b1b6bb7dfff4f1d09fbaafdcfe8437dc
StoyanDermendzhiev <techo01@abv.bg>
2021-02-08 18:55:54 +0200

    Upload

---
+++ C# Advanced/Exams/Exam-25October2020/01.Scheduling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                                                     .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                                     .Select(int.Parse));
+            Queue<int> threads = new Queue<int>(Console.ReadLine()
+                                                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                     .Select(int.Parse));
+            int killedValue = int.Parse(Console.ReadLine());
+
+            while (threads.Count > 0)
+            {
+                int currThread = threads.Peek();
+                int currTask = tasks.Peek();
+
+                if (currTask == killedValue)
+                {
+                    Console.WriteLine($"Thread with value {currThread} killed task {killedValue}");
+                    break;
+                }
+
+                if (currThread >= currTask)
+                {
+                    tasks.Pop();
+                }
+
+                threads.Dequeue();
+            }
+
+            Console.WriteLine(string.Join(" ", threads));
+        }
+    }
+}
