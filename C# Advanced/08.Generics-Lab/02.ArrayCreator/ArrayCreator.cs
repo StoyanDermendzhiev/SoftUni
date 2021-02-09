ef26de75b41b426e9c8c5dc9c74b477d301dff0c
StoyanDermendzhiev <techo01@abv.bg>
2021-02-09 20:23:57 +0200

    Upload

---
+++ C# Advanced/08.Generics-Lab/02.ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] genericArray = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                genericArray[i] = item;
+            }
+
+            return genericArray;
+        }
+    }
+}
