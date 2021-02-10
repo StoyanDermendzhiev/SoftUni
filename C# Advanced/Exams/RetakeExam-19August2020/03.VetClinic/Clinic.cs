06c7d57cd0ce891a10a24b9a3a39efbd661ad820
StoyanDermendzhiev <techo01@abv.bg>
2021-02-10 22:26:44 +0200

    Upload

---
+++ C# Advanced/Exams/RetakeExam-19August2020/03.VetClinic/Clinic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> pets;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            pets = new List<Pet>();
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => pets.Count;
+
+        public void Add(Pet pet)
+        {
+            if (pets.Count < Capacity)
+            {
+                pets.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet removedPet = pets.Find(p => p.Name == name);
+
+            if (removedPet != null)
+            {
+                pets.Remove(removedPet);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            return pets.FindAll(p => p.Owner == owner)
+                       .Find(p => p.Name == name);
+        }
+
+        public Pet GetOldestPet()
+        {
+            return pets.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (var pet in pets)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
