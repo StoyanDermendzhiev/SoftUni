2d54f68979f1f5b833aa0636ae6b7a16cd311ed9
StoyanDermendzhiev <techo01@abv.bg>
2021-02-10 20:05:47 +0200

    Upload

---
+++ C# Advanced/08.Generics-Exercises/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        private T value;
+
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{value.GetType()}: {value}";
+        }
+    }
+}
---
+++ C# Advanced/08.Generics-Exercises/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.GenericSwapMethodInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<int>> data = new List<Box<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<int> box = new Box<int>(int.Parse(Console.ReadLine()));
+                data.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, data));
+        }
+
+        public static void Swap<T>(List<T> data, int index1, int index2)
+        {
+            T current = data[index1];
+            data[index1] = data[index2];
+            data[index2] = current;
+        }
+    }
+}
