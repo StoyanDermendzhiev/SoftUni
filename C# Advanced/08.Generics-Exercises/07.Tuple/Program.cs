20f0bbdb22862fdc233bfbdb91af8d6d75d207c5
StoyanDermendzhiev <techo01@abv.bg>
2021-02-10 20:11:19 +0200

    Upload

---
+++ C# Advanced/08.Generics-Exercises/07.Tuple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+
+            string names = $"{firstInput[0]} {firstInput[1]}";
+            string addres = firstInput[2];
+
+            Tuple<string, string> first = new Tuple<string, string>(names, addres);
+
+            string[] secondInput = Console.ReadLine().Split();
+
+            string name = secondInput[0];
+            int beer = int.Parse(secondInput[1]);
+
+            Tuple<string, int> second = new Tuple<string, int>(name, beer);
+
+            string[] thirdInput = Console.ReadLine().Split();
+
+            int integerNum = int.Parse(thirdInput[0]);
+            double doubleNum = double.Parse(thirdInput[1]);
+
+            Tuple<int, double> third = new Tuple<int, double>(integerNum, doubleNum);
+
+            Console.WriteLine($"{first.Item1} -> {first.Item2}");
+            Console.WriteLine($"{second.Item1} -> {second.Item2}");
+            Console.WriteLine($"{third.Item1} -> {third.Item2}");
+        }
+    }
+}
