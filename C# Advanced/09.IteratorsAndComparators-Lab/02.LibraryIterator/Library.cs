c623b35e0fa1fda447d55d763f841d94028e023e
StoyanDermendzhiev <techo01@abv.bg>
2021-02-12 08:46:42 +0200

    Upload

---
+++ C# Advanced/09.IteratorsAndComparators-Lab/02.LibraryIterator/Library.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose(){}
+
+            public bool MoveNext() => ++currentIndex < books.Count;
+
+            public void Reset() => currentIndex = -1;
+        }
+    }
+}
