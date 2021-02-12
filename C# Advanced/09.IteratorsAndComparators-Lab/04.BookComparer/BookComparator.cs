b786ec04462386e02116896feeb748b5357fb24c
StoyanDermendzhiev <techo01@abv.bg>
2021-02-12 08:47:33 +0200

    Upload

---
+++ C# Advanced/09.IteratorsAndComparators-Lab/04.BookComparer/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public int Compare(Book one, Book two)
+        {
+            int result = one.Title.CompareTo(two.Title);
+
+            if (result == 0)
+            {
+                result = two.Year.CompareTo(one.Year);
+            }
+
+            return result;
+        }
+    }
+}
