045013dec95fe5012b04d83466c4deb0c77adb36
StoyanDermendzhiev <techo01@abv.bg>
2021-02-13 15:30:09 +0200

    Upload

---
+++ C# Advanced/09.IteratorsAndComparators-Exercises/04.Froggy/Lake.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> stones;
+
+        public Lake(List<int> stones)
+        {
+            this.stones = stones;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            Stack stack = new Stack();
+            Queue queue = new Queue();
+            
+            for (int i = 0; i < stones.Count; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                {
+                    stack.Push(stones[i]);
+                }
+                else
+                {
+                    queue.Enqueue(stones[i]);
+                }
+            }
+
+            foreach (int item in queue)
+            {
+                yield return item;
+            }
+
+            foreach (int item in stack)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/09.IteratorsAndComparators-Exercises/04.Froggy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine()
+                                        .Split(", ")
+                                        .Select(int.Parse)
+                                        .ToList());
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
