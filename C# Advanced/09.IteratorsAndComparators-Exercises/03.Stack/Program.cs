bc9197052096b422e2e34a4d0b3199310536b8a8
StoyanDermendzhiev <techo01@abv.bg>
2021-02-13 15:30:47 +0200

    Upload

---
+++ C# Advanced/09.IteratorsAndComparators-Exercises/03.Stack/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToArray();
+
+            MyStack<string> myStack = new MyStack<string>(firstInput);
+
+            string commandData = Console.ReadLine();
+
+            while (commandData != "END")
+            {
+                string[] command = commandData.Split();
+
+                if (command[0] == "Push")
+                {
+                    myStack.Push(command[1]);
+                }
+                else if (command[0] == "Pop")
+                {
+                    myStack.Pop();
+                }
+
+                commandData = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var item in myStack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/09.IteratorsAndComparators-Exercises/03.Stack/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class MyStack<T> : IEnumerable<T>
+    {
+        private List<T> items;
+
+        public MyStack(params T[] items)
+        {
+            this.items = items.ToList();
+        }
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Pop()
+        {
+            if (items.Count == 0)
+            {
+                Console.WriteLine("No elements");
+                return;
+            }
+
+            items.RemoveAt(items.Count - 1);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                yield return items[i];
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
