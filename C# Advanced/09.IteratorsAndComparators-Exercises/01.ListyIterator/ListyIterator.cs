5d0258687ba8e60dacc600842e11535ac9e7b4f5
StoyanDermendzhiev <techo01@abv.bg>
2021-02-13 15:32:07 +0200

    Upload

---
+++ C# Advanced/09.IteratorsAndComparators-Exercises/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public ListyIterator(params T[] items)
+        {
+            this.items = items.ToList();
+            index = 0;
+        }
+
+        public bool HasNext() => index < items.Count - 1;
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (items.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(items[index]);
+            }
+        }
+    }
+}
---
+++ C# Advanced/09.IteratorsAndComparators-Exercises/01.ListyIterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       
+            string[] createCommandItems = Console.ReadLine()
+                                            .Split()
+                                            .Skip(1)
+                                            .ToArray();
+
+            ListyIterator<string> list = new ListyIterator<string>(createCommandItems);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "Print":
+                        list.Print();
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
