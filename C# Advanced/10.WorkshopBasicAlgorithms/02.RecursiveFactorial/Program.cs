51ba77e2ba04ef56adf80699d097552f592332fb
StoyanDermendzhiev <techo01@abv.bg>
2021-02-18 13:04:36 +0200

    Upload

---
+++ C# Advanced/10.WorkshopBasicAlgorithms/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long result = Factorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
