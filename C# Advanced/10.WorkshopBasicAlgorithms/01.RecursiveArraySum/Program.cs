fbcbb08de1fcba52332206ba4abcdc133db580e6
StoyanDermendzhiev <techo01@abv.bg>
2021-02-18 13:04:14 +0200

    Upload

---
+++ C# Advanced/10.WorkshopBasicAlgorithms/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split()
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            long sum = Sum(numbers, 0);
+
+            Console.WriteLine(sum);
+
+        }
+
+        public static long Sum(int[] numbers, int index)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+
+            return numbers[index] + Sum(numbers, index + 1);
+        }
+    }
+}
