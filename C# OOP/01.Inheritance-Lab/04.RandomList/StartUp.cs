c01ed5e433ea0c9c034a8aaf95eaf00e76275a03
StoyanDermendzhiev <techo01@abv.bg>
2021-02-21 21:03:28 +0200

    Upload

---
+++ C# OOP/01.Inheritance-Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("Ivan");
+            list.Add("Dragan");
+            list.Add("Petkan");
+            list.Add("Gosho");
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
