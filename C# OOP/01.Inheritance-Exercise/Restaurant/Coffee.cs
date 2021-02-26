38a1af870704896000f223e118e01ddfe99f1a68
StoyanDermendzhiev <techo01@abv.bg>
2021-02-26 23:58:15 +0200

    Upload

---
+++ C# OOP/01.Inheritance-Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const decimal defualtPrice = 3.5M;
+        private const double defualtMilliliters = 50;
+
+        public Coffee(string name, double caffeine) 
+            : base(name, defualtPrice, defualtMilliliters)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
