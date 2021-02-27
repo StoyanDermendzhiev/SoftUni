f970b8445664c63274ed86bef47bf2dadb498381
StoyanDermendzhiev <techo01@abv.bg>
2021-02-27 21:21:54 +0200

    Upload

---
+++ C# OOP/01.Inheritance-Exercise/Animals/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+
+        public virtual string Sound { get; set; }
+
+        public virtual string ProduceSound()
+        {
+            return Sound;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}{Environment.NewLine}" +
+                   $"{Name} {Age} {Gender}{Environment.NewLine}" +
+                   $"{ProduceSound()}";
+        }
+    }
+}
---
+++ C# OOP/01.Inheritance-Exercise/Animals/StartUp.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string animal = Console.ReadLine();
+                {
+                    if (animal == "Beast!")
+                        break;
+                }
+
+                string[] animalInfo = Console.ReadLine().Split();
+
+                string name = animalInfo[0];
+                int age = int.Parse(animalInfo[1]);
+                string gender = animalInfo[2];
+
+                if (string.IsNullOrEmpty(name) ||
+                    age < 0 ||
+                    (gender != "Male" && gender != "Female"))
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                switch (animal)
+                {
+                    case "Dog":
+                        Dog dog = new Dog(name, age, gender);
+                        Console.WriteLine(dog);
+                        break;
+                    case "Cat":
+                        Cat cat = new Cat(name, age, gender);
+                        Console.WriteLine(cat);
+                        break;
+                    case "Frog":
+                        Frog frog = new Frog(name, age, gender);
+                        Console.WriteLine(frog);
+                        break;
+                    case "Kitten":
+                        Kitten kitten = new Kitten(name, age);
+                        Console.WriteLine(kitten);
+                        break;
+                    case "Tomcat":
+                        Tomcat tomcat = new Tomcat(name, age);
+                        Console.WriteLine(tomcat);
+                        break;
+                }
+
+                animal = Console.ReadLine();
+            }
+        }
+    }
+}
