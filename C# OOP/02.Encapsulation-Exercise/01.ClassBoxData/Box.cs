ba01ef7bf32c534df4a37f3d905ed5fdcfc7ae31
StoyanDermendzhiev <techo01@abv.bg>
2021-03-02 09:23:31 +0200

    Upload

---
+++ C# OOP/02.Encapsulation-Exercise/01.ClassBoxData/Box.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidOperationException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidOperationException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidOperationException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * length * height + 2 * width * height;
+        }
+
+        public double GetVolume()
+        {
+            return length * width * height;
+        }
+    }
+}
