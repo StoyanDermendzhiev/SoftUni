dd7cc20c72f6e2bb844c5812450c38fd3d758e66
StoyanDermendzhiev <techo01@abv.bg>
2021-03-13 13:16:32 +0200

    Upload

---
+++ C# OOP/04.Polymorphism-Lab/03.Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Circle(5.5);
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+
+            shape = new Rectangle(2, 3);
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
