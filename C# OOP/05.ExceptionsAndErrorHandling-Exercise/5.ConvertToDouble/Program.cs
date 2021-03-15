dc36308c170c7a849a9022b624e8a08ba3cde452
StoyanDermendzhiev <techo01@abv.bg>
2021-03-15 18:22:32 +0200

    Upload

---
+++ C# OOP/05.ExceptionsAndErrorHandling-Exercise/5.ConvertToDouble/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _5.ConvertToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double result;
+
+            try
+            {
+                result = Convert.ToDouble(input);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
