4a391f7463c710259700653a57d27e5572bb2348
StoyanDermendzhiev <techo01@abv.bg>
2021-03-15 18:20:50 +0200

    Upload

---
+++ C# OOP/05.ExceptionsAndErrorHandling-Exercise/1.SquareRoot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1.SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
