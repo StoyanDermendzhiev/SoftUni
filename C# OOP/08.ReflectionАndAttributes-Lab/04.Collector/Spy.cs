d05faa55d4acdd1c5ea87590fe29d37815c3c1af
StoyanDermendzhiev <techo01@abv.bg>
2021-03-27 17:07:47 +0200

    Upload

---
+++ C# OOP/08.ReflectionАndAttributes-Lab/04.Collector/Spy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string CollectGettersAndSetters(string className)
+        {
+            Type classType = Type.GetType(className);
+
+            MethodInfo[] classMethodInfos = classType.GetMethods(
+                BindingFlags.Instance |
+                BindingFlags.NonPublic |
+                BindingFlags.Public);
+
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (var method in classMethodInfos.Where(m=>m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (var method in classMethodInfos.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
