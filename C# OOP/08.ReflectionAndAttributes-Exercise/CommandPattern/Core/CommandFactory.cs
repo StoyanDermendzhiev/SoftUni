0c2ec17c181738518d61238f7a217ea0fc769124
StoyanDermendzhiev <techo01@abv.bg>
2021-03-27 17:11:19 +0200

    Upload

---
+++ C# OOP/08.ReflectionAndAttributes-Exercise/CommandPattern/Core/CommandFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandFactory : ICommandFactory
+    {
+        public ICommand CreateCommand(string commandType)
+        {
+           Type type = Assembly.GetEntryAssembly()
+                    .GetTypes()
+                    .FirstOrDefault(t => t.Name.StartsWith(commandType));
+
+           if (type == null)
+           {
+               throw new ArgumentException("Invalid command type");
+           }
+
+           return (ICommand)Activator.CreateInstance(type);
+        }
+    }
+}
---
+++ C# OOP/08.ReflectionAndAttributes-Exercise/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using CommandPattern.Commands;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private readonly ICommandFactory commandFactory;
+
+        public CommandInterpreter()
+        {
+            commandFactory = new CommandFactory();
+        }
+
+        public string Read(string args)
+        {
+            string[] parts = args.Split();
+
+            string commandType = parts[0];
+            string[] commandArgs = parts.Skip(1).ToArray();
+
+            ICommand command = commandFactory.CreateCommand(commandType);
+
+            return command.Execute(commandArgs);
+        }
+    }
+}
