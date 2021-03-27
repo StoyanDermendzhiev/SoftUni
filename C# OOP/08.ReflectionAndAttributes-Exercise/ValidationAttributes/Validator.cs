d18e964ee01b6de1fd4dcebe3a41682126049fca
StoyanDermendzhiev <techo01@abv.bg>
2021-03-27 17:10:40 +0200

    Upload

---
+++ C# OOP/08.ReflectionAndAttributes-Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] proprties = obj.GetType().GetProperties();
+
+            foreach (var property in proprties)
+            {
+                MyValidationAttribute[] attributes = property.GetCustomAttributes()
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                var value = property.GetValue(obj);
+
+                foreach (var attribute in attributes)
+                {
+                    bool isValid = attribute.IsValid(value);
+
+                    if (!isValid)
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
